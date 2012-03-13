c97c1bd87717ed14624b0b12b3c75137b84e9fbf
michitaka <michitaka.i@gmail.com>
2012-03-13 18:09:02 +0900

    fistCommit

---
+++ Hey/src/heyHey/heyBoy.java
@@ -0,0 +1,13 @@
+package heyHey;
+
+public class heyBoy {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi!");
+	}
+
+}
